a2fff5d0f821d485dfb4de1d8eca882cb29678f2
Luis Yanchapanta <yanchapanta2019@gmail.com>
2019-01-17 23:04:33 +0000

    displaying songs and title

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Is There More", "Emotionless", "Mob Ties", "Blue Tint", "Dont Matter to Me", "Final Fantasy" ];
+var images_links = ["http://thedjbenz.com/wp-content/uploads/2018/06/Drake%C2%B4s-Scorpion-album-is-released.jpg","https://upload.wikimedia.org/wikipedia/en/9/90/Scorpion_by_Drake.jpg", "http://thedjbenz.com/wp-content/uploads/2018/06/Drake%C2%B4s-Scorpion-album-is-released.jpg", "https://upload.wikimedia.org/wikipedia/en/9/90/Scorpion_by_Drake.jpg", "http://thedjbenz.com/wp-content/uploads/2018/06/Drake%C2%B4s-Scorpion-album-is-released.jpg", "https://upload.wikimedia.org/wikipedia/en/9/90/Scorpion_by_Drake.jpg"];
+var atrists = ["Drake", "Drake", "Drake", "Drake", "Drake", "Drake"];
+var songLengths = [];
+var links = [];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
+songs.forEach(function(item){
+    $("#songs").append("<ul>" + item + "</ul>");
+});
 
+images_links.forEach(function(image){
+    $("#images").append("<img src=" + image + ">");
+});
 
 
 
     // BELOW Use forEach Loop to display the data from each of your array's in the correct div
 
-}
 
+  
 function emptySongInfo(){
     $("#songs").empty();
     // Use jQuery to empty all of the remaining divs

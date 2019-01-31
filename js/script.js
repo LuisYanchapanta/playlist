e7143be25f81c4c7a7d840dbfc822c1477944d6d
Luis Yanchapanta <yanchapanta2019@gmail.com>
2019-01-31 23:32:52 +0000

    made columns for each song

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = ["Is There More", "Emotionless", "Mob Ties", "Blue Tint", "Dont Matter to Me", "Final Fantasy" ];
-var images_links = ["http://thedjbenz.com/wp-content/uploads/2018/06/Drake%C2%B4s-Scorpion-album-is-released.jpg","https://upload.wikimedia.org/wikipedia/en/9/90/Scorpion_by_Drake.jpg", "http://thedjbenz.com/wp-content/uploads/2018/06/Drake%C2%B4s-Scorpion-album-is-released.jpg", "https://upload.wikimedia.org/wikipedia/en/9/90/Scorpion_by_Drake.jpg", "http://thedjbenz.com/wp-content/uploads/2018/06/Drake%C2%B4s-Scorpion-album-is-released.jpg", "https://upload.wikimedia.org/wikipedia/en/9/90/Scorpion_by_Drake.jpg"];
+var songs = ["Is There More", "Emotionless", "Mob Ties", "Blue Tint", "Dont Matter to Me", "Final Fantasy"];
+var images_links = ["http://thedjbenz.com/wp-content/uploads/2018/06/Drake%C2%B4s-Scorpion-album-is-released.jpg", "https://upload.wikimedia.org/wikipedia/en/9/90/Scorpion_by_Drake.jpg", "http://thedjbenz.com/wp-content/uploads/2018/06/Drake%C2%B4s-Scorpion-album-is-released.jpg", "https://upload.wikimedia.org/wikipedia/en/9/90/Scorpion_by_Drake.jpg", "http://thedjbenz.com/wp-content/uploads/2018/06/Drake%C2%B4s-Scorpion-album-is-released.jpg", "https://upload.wikimedia.org/wikipedia/en/9/90/Scorpion_by_Drake.jpg"];
 var atrists = ["Drake", "Drake", "Drake", "Drake", "Drake", "Drake"];
 var songLengths = [];
 var links = [];
@@ -13,19 +13,43 @@ var links = [];
 //     $("#images").append("<img src=" + image + ">");
 // });
 
+var lastColumn;
 var i;
-for(i=0; i < songs.length; i++){
-    console.log(i);
-     $("#songs").append("<img src=" + images_links[i] + ">");
-    $("#songs").append("<ul>" + songs[i] + "</ul>");
+for (i = 0; i < songs.length; i++) {
+    //console.log(i);
+    //console.log(lastColumn);
+    if (i == 0) {
+        lastColumn = "#songs1";
+        // put song name in song1
+    }
+    else if (lastColumn == "#songs1") {
+        lastColumn = "#songs2";
+        // put song name in song2
+    }
+    else if (lastColumn == "#songs2") {
+        lastColumn = "#songs3";
+        // put song name in song3
+    }
+    else if (lastColumn == "#songs3") {
+        lastColumn = "#songs1";
+        // put song name in song1
+    }
+
+    //console.log(lastColumn + " " + songs[i]);
+    //$(lastColumn).append(songs[i]);
+    $(lastColumn).append("<ul>" + songs[i] + "</ul>");
+    $(lastColumn).append("<img class='coverSong' src=" + images_links[i] + ">");
+
+
+
 }
 
 
-    // BELOW Use forEach Loop to display the data from each of your array's in the correct div
+// BELOW Use forEach Loop to display the data from each of your array's in the correct div
 
 
-  
-function emptySongInfo(){
+
+function emptySongInfo() {
     $("#songs").empty();
     // Use jQuery to empty all of the remaining divs
 
@@ -33,7 +57,7 @@ function emptySongInfo(){
 }
 
 
-function addSongInfo(){
+function addSongInfo() {
     // BELOW write the code to add new items to each of the arrays.
 
 
